d65e44ec17722bed6c9a44969f1ad2f6885d0511
Sharon Lee <dasom.s.lee@gmail.com>
2022-03-16 23:04:32 +0900

    Create pyspark_cheatsheet.py

---
+++ pyspark_cheatsheet.py
@@ -0,0 +1,73 @@
+
+def shape(self):
+	"""
+	Prints the number of rows and columns in a spark DataFrame
+	"""
+
+	return self.count(), len(self.schema.names)
+pyspark.sql.DataFrame.shape = shape
+
+
+def info(self, show = True):
+  
+  '''
+  Print concise summary of a pyspark.sql.DataFrame
+  This method prints information about a DataFrame
+  including the index dtype and columns, non-null values
+  
+  Args:
+    show(bool): prints dataframe
+    
+  Returns:
+    pyspark.sql.DataFrame
+
+  '''
+  
+  subset = self.schema.names
+  total_rows = self.count()
+  _non_null = self.select([(total_rows - f.sum(f.when(f.col(col).isNull(),1).otherwise(0))).alias(col) for col in subset])\
+    .toPandas().transpose().reset_index().rename(columns={'index':'Column', 0:'Non-Null Count'})
+  _non_null = spark.createDataFrame(_non_null)
+  _dtype = spark.createDataFrame(self.dtypes).withColumnRenamed('_1','Column').withColumnRenamed('_2','Dtype')
+  result = _dtype.join(_non_null, on = 'Column').select('Column', 'Non-Null Count', 'Dtype')
+  
+  if show:
+    return result.show()
+  else:
+    return result
+pyspark.sql.DataFrame.info = info
+
+
+def value_counts(self, subset, normalize = True, sort = True, ascending = False, show = True):
+  '''
+  Prints frequencies and proportion of unique value in a DataFrame
+  
+  Args:
+    subset(list): column to be used.
+    normalize(bool): returns proportion
+    sort(bool): sort by frequencies
+    ascending(bool): sort in ascending order.
+    
+  Return:
+    pyspark.sql.DataFrame
+  '''
+  
+  w = Window.partitionBy().rowsBetween(Window.unboundedPreceding, Window.unboundedFollowing)
+  
+  self = self.groupby(subset).count()
+  
+  if normalize:
+    self = self.withColumn('pct', f.round(f.col('count')/f.sum('count').over(w),4))
+  else:
+    pass
+  
+  if sort:
+    self = self.sort('count', ascending = ascending)
+  else:
+    self = self.sort(subset)
+    
+  if show:
+    return self.show()
+  else:
+    return self
+pyspark.sql.DataFrame.value_counts = valueCounts
